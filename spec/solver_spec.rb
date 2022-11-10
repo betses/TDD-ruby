6e41959c0dbf46e90f99df767adff3017902e34c
aceDavon <makojidavid@gmail.com>
2022-11-10 11:09:22 +0100

    Fix linter error on test suites

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,48 +1,46 @@
 require_relative '../solver'
 
 describe Solver do
-
-    describe 'Test the factorial method of 5' do 
-      before(:each) do
-        @value = 5
-        @zero = 0
-      end
-
-      it 'check for factorial of 0' do
-        @funk = Solver.new
-        expect(@funk.factorial(@zero)).to eql 1
-      end
-      it 'check for factorial of -5' do
-        @funk = Solver.new
-        @value = -5
-        expect(@funk.factorial(@value)).not_to eql 1
-      end
-      it 'check for factorial of 5' do
-        @funk = Solver.new
-        expect(@funk.factorial(@value)).not_to eql 120
-      end
+  describe 'Test the factorial method of 5' do
+    before(:each) do
+      @value = 5
+      @zero = 0
     end
 
-    describe 'Test for reverse string method' do
-      it 'Check the reverse string of Hello' do
-        @solver = Solver.new
-        expect(@solver.reverse('Hello')).to eql 'olleh'
-      end
+    it 'check for factorial of 0' do
+      @funk = Solver.new
+      expect(@funk.factorial(@zero)).to eql 1
     end
-
-    describe 'Test fizzbuzz' do
-      it 'testing fizzbuzz for 15' do
-        @solver = Solver.new
-        expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
-      end
-      it 'testing fizzbuzz for 20' do
-        @solver = Solver.new
-        expect(@solver.fizzbuzz(20)).to eql 'buzz'
-      end
-      it 'testing fizzbuzz for 2' do
-        @solver = Solver.new
-        expect(@solver.fizzbuzz(2)).to eql '2'
-      end
+    it 'check for factorial of -5' do
+      @funk = Solver.new
+      @value = -5
+      expect(@funk.factorial(@value)).not_to eql 1
     end
+    it 'check for factorial of 5' do
+      @funk = Solver.new
+      expect(@funk.factorial(@value)).not_to eql 120
+    end
+  end
 
+  describe 'Test for reverse string method' do
+    it 'Check the reverse string of Hello' do
+      @solver = Solver.new
+      expect(@solver.reverse('Hello')).to eql 'olleh'
+    end
+  end
+
+  describe 'Test fizzbuzz' do
+    it 'testing fizzbuzz for 15' do
+      @solver = Solver.new
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+    it 'testing fizzbuzz for 20' do
+      @solver = Solver.new
+      expect(@solver.fizzbuzz(20)).to eql 'buzz'
+    end
+    it 'testing fizzbuzz for 2' do
+      @solver = Solver.new
+      expect(@solver.fizzbuzz(2)).to eql '2'
+    end
+  end
 end
